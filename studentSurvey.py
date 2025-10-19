79f88592e210b4c495268e82d5b9836bbc55b167
aleya566 <aleya.aelyana566@gmail.com>
2025-10-19 14:49:49 +0800

    Add metrics for PLO 2 to PLO 5
    Added metrics for PLO 2 to PLO 5 in the dashboard.

--- studentSurvey.py
+++ studentSurvey.py
@@ -21,6 +21,12 @@ def load_data():
 
 arts_df = load_data()
 
+col1, col2, col3, col4 = st.columns(4)
+col1.metric(label="PLO 2", value=f"3.3", help="PLO 2: Cognitive Skill", border=True)
+col2.metric(label="PLO 3", value=f"3.5", help="PLO 3: Digital Skill", border=True)
+col3.metric(label="PLO 4", value=f"4.0", help="PLO 4: Interpersonal Skill", border=True)
+col4.metric(label="PLO 5", value=f"4.3", help="PLO 5: Communication Skill", border=True)
+
 # --- Streamlit App Layout ---
 st.title("📊 Arts Faculty Data Visualization")
 st.markdown("This dashboard presents key distributions from the Arts Faculty dataset using Plotly.")
